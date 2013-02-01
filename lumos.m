3d5453587e26202f57db20daaf331bdff57764e8
Jesse Lu <mr.jesselu@gmail.com>
2013-02-01 09:34:30 -0800

    Mostly cleaned up lumos.
    Also added restart capability.

--- lumos.m
+++ lumos.m
@@ -5,8 +5,14 @@
 % Solves the problem specified, using the parameterization and 
 % paradigm desired.
 
-function [z, p, vis_result] = lumos(gen_problem, paradigm, param_type, ...
+function [z, p, vis_result] = lumos(name, gen_problem, paradigm, param_type, ...
                                 p0, num_iters, err_thresh, varargin)
+
+    %% Parse optional parameters.
+    lumos_options = struct( 'restart', false);
+    for k = 2 : 2 : length(varargin)
+        lumos_options = setfield(lumos_options, varargin{k-1}, varargin{k});
+    end
 
     %% Generate the problem
     % This includes the physics residual and field design objectives,
@@ -28,8 +34,6 @@ function [z, p, vis_result] = lumos(gen_problem, paradigm, param_type, ...
                     'starting_iter', 1, ...
                     'num_iters', num_iters, ...
                     'err_thresh', err_thresh, ...
-                    'state_file', 'ex2D_state.mat', ...
-                    'history_file', 'ex2D_history.h5', ...
                     'vis_progress', @my_track_progress);
 
     if strcmp(paradigm, 'global')
@@ -47,11 +51,10 @@ function [z, p, vis_result] = lumos(gen_problem, paradigm, param_type, ...
     elseif strcmp(paradigm, 'local')
         options.paradigm_args = {   'initial_kappa', 1e0, ...
                                     'kappa_growth_rate', 1.3, ...
-                                    'kappa_shrink_rate', 0.5};
+                                    'kappa_shrink_rate', 0.3};
 
         options.structure_args = {  'step_err', 1e-4, ...
-                                    'err_thresh', 1e-3, ...
-                                    'tidyup_p', tidyup_p};
+                                    'err_thresh', 1e-3};
     else
         error('Invalid paradigm.');
     end
@@ -75,19 +78,22 @@ function [z, p, vis_result] = lumos(gen_problem, paradigm, param_type, ...
                             'w', @(p) 0, ...
                             'p_range', ones(opt_prob.z_len, 1) * [0 1], ...
                             'scheme', 'continuous-linear');
-        options.structure_args.tidyup_p = @(p) p;
+        options.structure_args = [options.structure_args, ...
+                                    {'tidyup_p', @(p) p}];
 
     elseif strcmp(param_type, 'level-set') % Level-set parameterization.
         struct_obj = struct('m', @(p) vec(phi2p(unvec(p), [0 1])), ...
                             'w', @(p) 0, ...
                             'p_range', ones(opt_prob.z_len, 1) * [-1 1], ...
                             'scheme', 'continuous');
-        options.structure_args.tidyup_p = @(p) vec(smooth_phi(unvec(p), 0));
+        options.structure_args = [options.structure_args, ...
+                            {'tidyup_p', @(p) vec(smooth_phi(unvec(p), 0))}];
     else
         error('Invalid type of parameterization');
     end
 
 
+    %% Get initial parameterization values
 
     if isempty(p0)
         p0 = max(struct_obj.p_range, [], 2);
@@ -95,32 +101,28 @@ function [z, p, vis_result] = lumos(gen_problem, paradigm, param_type, ...
         p0 = p0 * ones(size(struct_obj.p_range, 1), 1);
     end
 
-%     if ~isempty(varargin)
-%         opt_state = load(varargin{1});
-%         opt_prob = opt_state.opt_prob;
-%         g = opt_state.g;
-%         p0 = opt_state.p;
-%         options = opt_state.options;
-%         options.starting_iter = opt_state.k + 1;
-%         options.num_iters = num_iters;
-%         state = opt_state.state;
-%         progress = opt_state.progress;
-%     else
-%         state = [];
-%         progress = [];
-%     end
+    %% Take care of restart, if necessary
+    options.state_file = ['results/', name, '_state.mat'];
+    options.history_file = ['results/', name, '_history.h5'];
+
+    if lumos_options.restart
+        opt_state = load(options.state_file);
+        p0 = opt_state.p;
+        options.starting_iter = opt_state.k + 1;
+        state = opt_state.state;
+        progress = opt_state.progress;
+    else
         state = [];
         progress = [];
+    end
 
     %% Optimize
-    [z, p, state] = run_optimization(opt_prob, struct_obj, p0, options, ...
-                                        state, progress);
+    [z, p, state, progress] = run_optimization(opt_prob, struct_obj, p0, ...
+                                                options, state, progress);
 
 
     %% Visualize.
-    vis_result = @() track_progress(opt_prob, struct_obj, ...
-                                    vis_options.vis_layer, ...
-                                    vis_options.mode_sel, [], [], z, p);
+    vis_result = @() my_track_progress([], [], z, p, progress);
 
 end
 
