62f3563cd07adc30f4b336a2e59ba52dd079ccc4
Jesse <mr.jesselu@gmail.com>
2013-02-05 12:01:43 -0800

    Starting to work on spl_wdm.

--- problems/spl_wdm.m
+++ problems/spl_wdm.m
@@ -1,4 +1,4 @@
-function [problem] = spl_tetm(custom_model_options)
+function [problem] = spl_wdm(custom_model_options)
 
     % Choose the structure of the model (what waveguides to use where).
     model_structure = {'single', 'single', 'single'};
@@ -10,10 +10,10 @@ function [problem] = spl_tetm(custom_model_options)
     % out{1} = io(2, 'te0', [0.9 1]);
     out{1} = {io(2, 'te0', [0.9 1]), io(3, 'te0', [0 0.01])};
 
-    omega{2} = 2 * pi / 40;
-    in{2} = io(1, 'tm0', 1);
-    % out{2} = io(3, 'tm0', [0.9 1]);
-    out{2} = {io(2, 'tm0', [0 0.01]), io(3, 'tm0', [0.9 1])};
+    omega{2} = 2 * pi / 39;
+    in{2} = io(1, 'te0', 1);
+    % out{2} = io(3, 'te0', [0.9 1]);
+    out{2} = {io(2, 'te0', [0 0.01]), io(3, 'te0', [0.9 1])};
 
     vis_options.mode_sel = 1 : N;
 
