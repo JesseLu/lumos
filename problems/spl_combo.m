1bff3ef39acf465dc4090e5116fce6b8cd500178
Ubuntu <ubuntu@ip-10-0-0-250.(none)>
2013-02-08 19:09:11 +0000

    Changed vis_modes to 4 for spl_combo.

--- problems/spl_combo.m
+++ problems/spl_combo.m
@@ -16,7 +16,7 @@ function [problem] = spl_wdm(custom_model_options)
         out = out{port_num}; % Don't use reject bands.
     end
 
-    N = 1; % Number of modes.
+    N = 4; % Number of modes.
 
     omega{1} = 2 * pi / 38.75;
     in{1} = io(1, 'te0', 1);
