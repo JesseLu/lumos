98fb42a7c96e5c2990b775fa83a397782ad8ac1e
Ubuntu <ubuntu@ip-10-0-0-250.(none)>
2013-02-10 23:25:49 +0000

    Lengthened model_V2 for spl_combo.

--- problems/models/model_V2.m
+++ problems/models/model_V2.m
@@ -8,7 +8,7 @@ function [mode, vis_layer] = model_V2(omega, in, out, wg_types, options)
 % At the same time, make the in and out fields easier to specify.
 
     % Basic dimensions.
-    dims = [120 145 40];
+    dims = [145 145 40];
 
     wg_dirs = {'+', '-', '-', '-', '-'};
     wg_ypos = {dims(2)/2, 32, 59, 86, 113};
