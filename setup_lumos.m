ca5dbbf477de7f4d39226d7ada4a2834ddbcdb47
Ubuntu <ubuntu@ip-10-0-0-250.(none)>
2013-02-11 15:52:28 +0000

    More redundancy.

--- setup_lumos.m
+++ setup_lumos.m
@@ -6,4 +6,15 @@
     path(path, genpath(lumos_root_dir));
 
     %% Install maxwellFDS (alpha).
-    eval(urlread('http://m.lightlabs.co/alpha'));
+    % If this fails - try, try again.
+    for i = 1 : 20
+        try
+            eval(urlread('http://m.lightlabs.co/alpha'));
+            break;
+        catch exception
+            fprintf(getReport(exception, 'extended'));
+            fprintf('\n');
+            pause(rand(1)*10);
+            continue;
+        end
+    end
