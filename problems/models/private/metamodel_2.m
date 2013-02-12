b65b782fd9a4e2390ba40d9a364396a2abe1135d
Jesse <mr.jesselu@gmail.com>
2013-02-12 11:18:21 -0800

    Trying out 3D fib_te run.

--- problems/models/private/metamodel_2.m
+++ problems/models/private/metamodel_2.m
@@ -8,7 +8,7 @@ function [mode, vis_layer] = metamodel_2(dims, omega, in, out, ...
     pml_thickness = [10 10 10];
 
     if model_options.size == 'large'
-        size_boost = 20;
+        size_boost = 0;
     elseif model_options.size == 'small'
         size_boost = 0;
     end
@@ -29,11 +29,11 @@ function [mode, vis_layer] = metamodel_2(dims, omega, in, out, ...
 
     fiber_radius = 25;
     fiber_eps = 2.56;
-    fiber_z_center = 3*dims(3)/4;
-    fiber_z_thickness = dims(3)/2 + 2;
+    fiber_z_center = (dims(3) + z_center)/2;
+    fiber_z_thickness = (dims(3) - z_center) + 2;
 
-    sel_z_thickness = z_thickness/2;
-    sel_z_center = z_center + z_thickness/2 - sel_z_thickness/2;
+    sel_z_thickness = z_thickness;
+    sel_z_center = z_center;
 
 
     mu = {ones(dims), ones(dims), ones(dims)};
@@ -71,6 +71,13 @@ function [mode, vis_layer] = metamodel_2(dims, omega, in, out, ...
                         'J_shift', [0 0 0], ...
                         'E_shift', [0 0 -1]);
 
+    % Construct back-reflector.
+    back_reflector = struct('type', 'rectangle', ...
+                            'position', [0 0], ...
+                            'size', [1e9 1e9], ...
+                            'permittivity', -20);
+    epsilon = add_planar(epsilon, 1, (dims(3)/2 - 20)*2, {back_reflector});
+
 
 
     % Construct in-plane waveguides and modes.
@@ -99,10 +106,12 @@ function [mode, vis_layer] = metamodel_2(dims, omega, in, out, ...
     epsilon = add_planar(epsilon, z_center, z_thickness, {background, wg{:}, bottom});
 
 %     for k = 1 : 3
-%         subplot(3, 1, k);
-%         imagesc(abs(squeeze(epsilon{k}(:,:,:)))'); 
+%         subplot(2, 3, k);
+%         imagesc(abs(squeeze(epsilon{k}(:,:,round(dims(3)/2))))'); 
 %         axis equal tight;
-%         colorbar;
+%         subplot(2, 3, 3+k);
+%         imagesc(abs(squeeze(epsilon{k}(:,round(dims(2)/2),:)))'); 
+%         axis equal tight;
 %     end
 %     pause
 
@@ -124,16 +133,16 @@ function [mode, vis_layer] = metamodel_2(dims, omega, in, out, ...
                                     design_pos, ...
                                     reset_eps_val, sel_z_center, sel_z_thickness);
 
-    % Fancy fix.
-    % Gets rid of intermediate layer values for the assymetric structure.
-    n = prod(dims);
-    vec = @(f) [f{1}(:); f{2}(:); f{3}(:)];
-    unvec = @(v) {reshape(full(v(1:n)), dims), ...
-                    reshape(full(v(n+1:2*n)), dims), ...
-                    reshape(full(v(2*n+1:3*n)), dims)};
-    delta_epsilon = vec(epsilon_orig) - ...
-                    (vec(epsilon) + (eps_hi - eps_lo) * S * ones(size(S,2), 1));
-    epsilon = unvec(vec(epsilon) + abs(delta_epsilon));
+%     % Fancy fix.
+%     % Gets rid of intermediate layer values for the assymetric structure.
+%     n = prod(dims);
+%     vec = @(f) [f{1}(:); f{2}(:); f{3}(:)];
+%     unvec = @(v) {reshape(full(v(1:n)), dims), ...
+%                     reshape(full(v(n+1:2*n)), dims), ...
+%                     reshape(full(v(2*n+1:3*n)), dims)};
+%     delta_epsilon = vec(epsilon_orig) - ...
+%                     (vec(epsilon) + (eps_hi - eps_lo) * S * ones(size(S,2), 1));
+%     epsilon = unvec(vec(epsilon) + abs(delta_epsilon));
 
 
     %% Specify modes
