7fc6d8dbcaf8926d3d45bc29089c8ac5e9f2462a
Jesse <mr.jesselu@gmail.com>
2013-02-18 14:24:25 -0800

    Tried an unsuccessful f2f.

--- problems/models/model_U.m
+++ problems/models/model_U.m
@@ -1,5 +1,5 @@
-%% model_Y
-% ports above and below (free-space).
+%% model_U
+% ports above and below (fiber).
 
 %% Description
 % Allows 10 modes for full 3D, 6 modes for 2D.
@@ -11,7 +11,7 @@ function [mode, vis_layer] = model_U(omega, in, out, wg_types, options)
 % At the same time, make the in and out fields easier to specify.
 
     % Number of input and output ports.
-    dims = [30 30 40]; 
+    dims = [120 120  32]; 
     if options.flatten
         dims(2) = 1;
         sim_length = 1;
